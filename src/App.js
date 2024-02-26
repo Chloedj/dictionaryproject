07cfe4ba418812bc2c11278f7b5c55f723c8ca1d
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-02-26 19:30:34 +0000

    added basic header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="app-logo img-fluid" alt="app-logo" />
         <h1>Dictionary</h1>
       </header>
     </div>
