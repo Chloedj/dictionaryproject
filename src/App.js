bd5cfcf1448544912d8b552ef85730afd5749093
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-02-27 20:36:51 +0000

    Added basic search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
 import logo from './logo.png';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
         <img src={logo} className="app-logo img-fluid" alt="app-logo" />
         <h1>Dictionary</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+         This project was coded by{" "}
+        <a href="https://github.com/Chloedj" target="_blank" rel="noreferrer"
+          >Chloe Duggan-Jones</a
+        >
+        {" "}, is open souced on{" "}
+        <a
+          href="https://github.com/Chloedj/dictionaryproject"
+          target="_blank" rel="noreferrer"
+        >
+          on GitHub</a
+        >
+        ,{" "}and{" "}
+        <a
+          href="https://visionary-druid-e7d5a5.netlify.app/"
+          target="_blank" rel="noreferrer"
+          >hosted on Netlify</a
+        >
+      </footer>
+      </div>
     </div>
   );
 }
